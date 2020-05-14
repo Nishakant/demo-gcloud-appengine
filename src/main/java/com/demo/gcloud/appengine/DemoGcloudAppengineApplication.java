4cf290d13788c103e5c095f85ba0d4ae1eca4154
Nishakant Pandey <nishakant.fsd@gmail.com>
2020-05-14 19:34:38 +0530

    modifying

---
+++ src/main/java/com/demo/gcloud/appengine/DemoGcloudAppengineApplication.java
@@ -0,0 +1,13 @@
+package com.demo.gcloud.appengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGcloudAppengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGcloudAppengineApplication.class, args);
+	}
+
+}
